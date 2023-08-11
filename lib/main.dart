19d8e70653b2fa14db7f21aac684338bed0d3186
Muhammed hisham <hishamkool@gmail.com>
2023-08-12 00:49:24 +0530

    added images and also added on tap to route to details page

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,283 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+import 'detailsHotel.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: restaurentSearchUi(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+final List rooms = [
+  {"image": "assets/images/hotelRooms/room1.jpg", "title": "Room 1"},
+  {"image": "assets/images/hotelRooms/room2.jpg", "title": "Room 2"},
+  {"image": "assets/images/hotelRooms/room3.jpg", "title": "Room 3"},
+  {"image": "assets/images/hotelRooms/room4.jpg", "title": "Room 4"},
+];
 
-  // This widget is the root of your application.
+class restaurentSearchUi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 180,
+            backgroundColor: Colors.cyan,
+            leading: IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {},
+            ),
+            actions: [
+              IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+            ],
+            floating: true,
+            flexibleSpace: ListView(
+              children: [
+                SizedBox(
+                  height: 70,
+                ),
+                Text(
+                  'Enter your location..',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 20),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(40)),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Calicut, Kerala",
+                      border: InputBorder.none,
+                      icon: IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.search),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: SizedBox(
+              height: 10,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: buildCatogeries(),
+          ),
+          SliverList(
+            delegate:
+                SliverChildBuilderDelegate((BuildContext context, int index) {
+              return sliverListRooms(context, index);
+            }, childCount: 40),
+          )
+        ],
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+
+  Widget buildCatogeries() {
+    return Container(
+      // color: Colors.blueGrey,
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: 15.0,
+          ),
+          Category(
+            iconCategory: Icons.hotel,
+            titleCategory: 'Hotels',
+            bgColourCategory: Colors.redAccent,
+          ),
+          SizedBox(
+            width: 15.0,
+          ),
+          Category(
+            iconCategory: Icons.restaurant,
+            titleCategory: 'Restaurant',
+            bgColourCategory: Colors.blue,
+          ),
+          SizedBox(
+            width: 15.0,
+          ),
+          Category(
+            iconCategory: Icons.local_cafe,
+            titleCategory: 'Cafe',
+            bgColourCategory: Colors.orange,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget sliverListRooms(BuildContext context, int index) {
+    var room = rooms[index % rooms.length];
+    return InkWell(
+      // onTap: () =>detailsPage() ,
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => detailsPage(),));
+      },
+      child: Container(
+        margin: EdgeInsets.all(20.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(5.0),
+          child: Container(
+            child: Material(
+              elevation: 5.0,
+              borderRadius: BorderRadius.circular(5.0),
+              color: Colors.greenAccent,
+              child: Column(
+                children: [
+                  Stack(
+                    children: [
+                      Image.asset(room['image']),
+                      Positioned(
+                          right: 10,
+                          top: 10,
+                          child: Icon(
+                            Icons.star,
+                            color: Colors.grey.shade800,
+                            size: 20.0,
+                          )),
+                      Positioned(
+                          right: 8,
+                          top: 8,
+                          child: Icon(
+                            Icons.star_border,
+                            color: Colors.white,
+                            size: 24.0,
+                          )),
+                      Positioned(
+                          bottom: 20.0,
+                          right: 10.0,
+                          child: Container(
+                            padding: EdgeInsets.all(10.0),
+                            color: Colors.white,
+                            child: Text('\₹1200'),
+                          )),
+                    ],
+                  ),
+                  Container(
+                    // color: Colors.blueAccent,
+                    padding: EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          room['title'],
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        Text('Kakkavayal, India'),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.star,
+                              color: Colors.green,
+                            ),
+                            Icon(
+                              Icons.star,
+                              color: Colors.green,
+                            ),
+                            Icon(
+                              Icons.star,
+                              color: Colors.green,
+                            ),
+                            Icon(
+                              Icons.star,
+                              color: Colors.green,
+                            ),
+                            Icon(
+                              Icons.star,
+                              color: Colors.green,
+                            ),
+                            SizedBox(
+                              width: 5.0,
+                            ),
+                            Text(
+                              '(229 reviews)',
+                              style: TextStyle(color: Colors.black26),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class Category extends StatelessWidget {
+  final IconData iconCategory;
+  final String titleCategory;
+  final Color? bgColourCategory;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  const Category(
+      {Key? key,
+      required this.iconCategory,
+      required this.titleCategory,
+      this.bgColourCategory})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+    return InkWell(
+      //or use gestureDetector for ontap funtionality
+      onTap: () {},
+      child: Container(
+        decoration: BoxDecoration(
+          color: bgColourCategory,
+          borderRadius: BorderRadius.all(Radius.circular(15)),
+        ),
+        margin: EdgeInsets.symmetric(vertical: 5.0),
+        padding: EdgeInsets.all(10.0),
+        width: 100,
+        height: 100,
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Icon(
+              iconCategory,
+              color: Colors.white,
+              size: 30,
+            ),
+            SizedBox(
+              height: 5.0,
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              titleCategory,
+              style: TextStyle(color: Colors.white),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+//name hotelRestaurentCafeUI.dart
